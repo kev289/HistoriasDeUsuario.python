dc93a0d799427cfe617d99090614b28866586d8b
KevinUribe <kuriwi.777@gmail.com>
2025-11-19 11:22:30 -0500

    userhistory2

--- HDU_1.py
+++ HDU_1.py
@@ -1,14 +1,16 @@
-# Intenta toda la linea de codigo para que si algun dato no esta en el parametro lance el except e imprima dato incorrecto
-try:
-    #Declaro las variables y le solicito al usuario los datos de el producto 
-    nombre = input("Ingrese el nombre del producto: ")
-    precio = float(input("Ingrese el precio del producto: "))
-    cantidad = int(input("Ingrese la cantidad del producto: "))
-    costo_total = precio * cantidad  
+#Coloco un while true para que me ejecute el codigo hasta que el usuario coloque los datos correctos
+while True:
+    #Hago un try para que ejecute el codigo desde que los datos proporcionados por el usuario esten correctos 
+    try:
+        nombre = input("Ingrese el nombre del producto: ")
+        precio = float(input("Ingrese el precio del producto: "))
+        cantidad = int(input("Ingrese la cantidad del producto: "))
+        costo_total = precio * cantidad  
 
-    print(f"Nombre:", nombre, "Precio:", precio, "Cantidad:", cantidad, "Costo total: ", costo_total  )
-    
-except  ValueError: 
-    print("Dato Incorrecto") 
+        print(f"Nombre:", nombre, "Precio:", precio, "Cantidad:", cantidad, "Costo total: ", costo_total  )
+        break
+        #El except es para dicho caso en el que el usuario proporcione un dato incorrecto, me imprime datos incorrectos.
+    except  ValueError: 
+        print("Dato Incorrecto ingrese un valor valido") 
 
-#Es un inventario donde imprime el nombre, precio, cantidad y costo total.
+
--- HDU_2.py
+++ HDU_2.py
@@ -1,3 +1,61 @@
-print("1. Agregar un producto. 2. Mostrar un producto: 3. Calcular estadisticas. 4. Salir. ")
-option = int(input("Ingresa una opcion: "))
+#Inventario es donde se almacenan los datos proporcionados por el usuario
+inventario = []
+#Funcion numero 1 destinada para anadir productos
+def añadir_producto():
+    while True:
+        try:
+            producto = input("Ingresa el nombre del producto: ")
+            precio = float(input("Ingresa el precio del producto: "))
+            cantidad = int(input("Ingresa la cantidad del producto: ")) 
 
+            tempInventario = {"producto": producto,
+                               "precio": precio, 
+                               "cantidad": cantidad,
+                               }
+            
+            inventario.append(tempInventario)
+
+            print(f"{producto} se ingreso con exito")
+
+            break 
+        except ValueError:
+            print("Ingresa valores validos")
+#Funcion numero 2 destinado para mostrar productos
+def mostrar_producto():
+    if not inventario:
+        print("El inventario esta vacio")
+    else: 
+        for item in inventario:
+            print(f"producto: {item['producto']}| cantidad: {item['cantidad']}| precio: {item['precio']} ")
+        
+#Funcion numero 3 destinada para mostrar un costo total
+def costo_total():
+    
+    for item in inventario:
+        subtotal = item['precio'] * item['cantidad']
+        print(f"El subtotal a pagar de {item['producto']} es: {subtotal}")
+
+    total = 0
+    for i in inventario:
+        total += i['precio'] * i['cantidad']
+    
+    print(f"El total a pagar por los productos es ${total}")
+            
+#Bucle para seguir ejecutando el codigo hasta que el usuario seleccione la opcion 4 y salir de el menu
+while True:
+    print("1. Agregar un producto. 2. Mostrar un producto: 3. Costo total. 4. Salir. ")
+    option = input("Ingresa una opcion: ")
+
+    if option == "1":
+        añadir_producto()
+    elif option == "2": 
+        mostrar_producto()
+    elif option == "3": 
+        costo_total()
+    elif option == "4": 
+        print("Hasta luego")
+        break
+#else para que si el usuario ingresa un dato invalida me imprima que lo ingrese bien
+    else: 
+        print("Error ingrese una opcion valida")
+                
